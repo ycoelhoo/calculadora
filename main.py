7604bb5a7f2344126bc53c0585db1b91d2854218
ycoelhoo <anonymossilence@gmail.com>
2021-05-27 10:39:19 -0300

    codigo-calculadora

---
+++ main.py
@@ -0,0 +1,18 @@
+print ('Vamos iniciar a soma')
+import time
+time.sleep(2) #sleep for 3 seconds
+
+n1 = input('Digite o primeiro número: ')
+n2 = input('Digite o segundo número: ')
+soma = int(n1) + int (n2)
+print ('O resultado da soma é : {valor}'.format(valor=soma))
+
+time.sleep(2) #sleep for 3 seconds
+
+print ('\n\nagora faremos a divisão')
+n1 = int(input('Digite o primeiro número: '))
+n2 =int(input('Digite o segundo número: '))
+print(f' a Divisão entre {n1} e {n2} é: {n1 / n2}')
+  
+
+
